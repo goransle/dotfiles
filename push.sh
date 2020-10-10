ebd7ec65e901e4e88ba1ae065aecff1adf12effa
Gøran A. Slettemark <goransle@gmail.com>
2020-10-10 07:24:32 +0200

    Pushed Sat Oct 10 07:24:32 CEST 2020

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-cp -R ~/.config/nvim ./nvim;
-cp ~/.zshrc ./zshrc;
-cp -R ~/.vim ./vim;
+cp -R ~/.config/nvim ./;
+cp ~/.zshrc ./;
+cp -R ~/.vim ./;
 git add *;
 git commit -m "Pushed `date`";
 git push origin master;
