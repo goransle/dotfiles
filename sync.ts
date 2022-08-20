f0b244319522e66548e7eb412fc90992ce5d499c
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2022-08-20 17:58:03 +0200

    Update 2022-08-20T15:58:03.311Z

--- sync.ts
+++ sync.ts
@@ -2,7 +2,7 @@ import * as fs from "https://deno.land/std@0.152.0/fs/mod.ts";
 import { join, relative } from "https://deno.land/std@0.152.0/path/posix.ts";
 import home_dir from "https://deno.land/x/dir@1.5.1/home_dir/mod.ts";
 
-
+const { args } = Deno;
 
 const { run } = Deno;
 
@@ -16,14 +16,15 @@ async function addFile(path: string) {
 
 }
 
-
 const toSync = [
     '.config/nvim',
     '.zshrc'
 ];
 
-await Promise.all(toSync.map(path => addFile(path)));
+if (args.includes('push')) {
+    await Promise.all(toSync.map(path => addFile(path)));
 
-const commit = run({ cmd: ['git', 'commit', '-m', 'test'] });
+    const commit = run({ cmd: ['git', 'commit', '-m', 'Update ' + (new Date).toISOString()] });
 
-await commit.status();
+    await commit.status();
+}
