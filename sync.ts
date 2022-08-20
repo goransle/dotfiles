64575c8b861c8cfc0701e909982e10b491467998
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2022-08-20 18:03:08 +0200

    Update 2022-08-20T16:03:08.052Z

--- sync.ts
+++ sync.ts
@@ -1,4 +1,4 @@
-import * as fs from "https://deno.land/std@0.152.0/fs/mod.ts";
+import {copy} from "https://deno.land/std@0.152.0/fs/mod.ts";
 import { join, relative } from "https://deno.land/std@0.152.0/path/posix.ts";
 import home_dir from "https://deno.land/x/dir@1.5.1/home_dir/mod.ts";
 
@@ -6,10 +6,11 @@ const { args } = Deno;
 
 const { run } = Deno;
 
-async function addFile(path: string) {
-    const home = home_dir() || ''
+const home = home_dir() || '';
 
-    await fs.copy(join(home, path), join('./', path), { overwrite: true });
+async function addFile(path: string) {
+
+    await copy(join(home, path), join('./', path), { overwrite: true });
 
     const command = run({ cmd: ['git', 'add', '.'] })
     await command.status();
@@ -18,13 +19,19 @@ async function addFile(path: string) {
 
 const toSync = [
     '.config/nvim',
-    '.zshrc'
+    '.zshrc',
+    'bunches'
 ];
 
 if (args.includes('push')) {
     await Promise.all(toSync.map(path => addFile(path)));
-
     const commit = run({ cmd: ['git', 'commit', '-m', 'Update ' + (new Date).toISOString()] });
 
     await commit.status();
+}
+
+if (args.includes('pull')) {
+    await Promise.all(toSync.map(path => {
+        return copy(join('./', path), join(home, path), { overwrite: true })
+    }));
 }
