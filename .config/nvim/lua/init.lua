f3d677146f09d50bf9547848f2be6860a5da1d81
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2023-01-29 08:36:25 +0100

    Update 2023-01-29T07:36:25.829Z

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -120,16 +120,3 @@ lsp.svelte.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lsp.emmet_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-  init_options = {
-    html = {
-      options = {
-        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-        ["bem.enabled"] = true,
-      },
-    },
-  }
-})
